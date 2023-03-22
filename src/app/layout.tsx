3c794e5cf7f47e4801e165f6499aa3eb2ab0efb1
Scott Gibbs <scott.gibbs@autocode.com>
2023-03-22 17:31:22 +0100

    Updated head config to show the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,17 @@
-import Head from "next/head"
+import type { Metadata } from "next"
 import { PropsWithChildren } from "react"
 import "./globals.css"
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Panda Snap",
   description:
     "Snapshot designs that inspire you and create a personalised collection. Design your next product faster but browsing them for inspiration.",
+  authors: [{ name: "@kanga_bru" }],
 }
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <Head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-        <meta
-          name="keywords"
-          content="design, inspiration, panda, snap, chrome, firefox, extension"
-        />
-        <meta name="author" content="@kanga_bru" />
-      </Head>
       <html lang="en" className="h-full">
         <body>{children}</body>
       </html>
