48791f9e6cceef52dee0ef6ce3b5c3c3fa68e686
Scott Gibbs <scott.gibbs@autocode.com>
2023-04-05 17:56:42 +0200

    Updated seo tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,35 @@ import type { Metadata } from "next"
 import { PropsWithChildren } from "react"
 import "./globals.css"
 
+import promoCover from "public/images/promo-cover.jpg"
+
+const title = "Panda Snap"
+const description =
+  "Snapshot designs that inspire you and create a personalised collection. Design your next product faster but browsing them for inspiration."
+const twitterTag = "@kanga_bru"
+
 export const metadata: Metadata = {
-  title: "Panda Snap",
-  description:
-    "Snapshot designs that inspire you and create a personalised collection. Design your next product faster but browsing them for inspiration.",
-  authors: [{ name: "@kanga_bru" }],
+  title,
+  description,
+  authors: [{ name: twitterTag }],
+  twitter: {
+    title,
+    description,
+    card: "summary_large_image",
+    creator: twitterTag,
+    images: [promoCover.src],
+  },
+  openGraph: {
+    title,
+    description,
+    images: [
+      {
+        url: promoCover.src,
+        height: promoCover.height,
+        width: promoCover.width,
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }: PropsWithChildren) {
